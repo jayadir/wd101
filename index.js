8debf8562239a5632895975fa731398741e545e8
jayadir <140229750+jayadir@users.noreply.github.com>
2023-11-20 23:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,85 @@
+function formatUserDate(inputDate) {
+    const parts = inputDate.split('/');
+    if (parts.length !== 3) {
+        return 'Invalid date format';
+    }
 
-function submitForm(event) {
-  event.preventDefault();
-  const fullNameInput = document.getElementById("name");
-  const emailAddressInput = document.getElementById("email");
-  const passwordInput = document.getElementById("password");
-  const dateOfBirthInput = document.getElementById("dob");
-  const acceptTermsInput = document.getElementById("acceptTerms");
-
-  if (!validateEmail(emailAddressInput.value)) {
-    alert("Please enter a valid email address.");
-    return false;
-  }
-
-  if (!validateAge(dateOfBirthInput.value)) {
-    alert("Your age must be between 18 and 55");
-    return false;
-  }
-
-  const tableBody = document.querySelector("#userTable tbody");
-  const row = tableBody.insertRow();
-  const fullNameCell = row.insertCell(0);
-  const emailAddressCell = row.insertCell(1);
-  const passwordCell = row.insertCell(2);
-  const dateOfBirthCell = row.insertCell(3);
-  const acceptTermsCell = row.insertCell(4);
-
-  fullNameCell.textContent = fullNameInput.value;
-  emailAddressCell.textContent = emailAddressInput.value;
-  passwordCell.textContent = passwordInput.value;
-  dateOfBirthCell.textContent = dateOfBirthInput.value;
-  acceptTermsCell.textContent = acceptTermsInput.checked ? "Yes" : "No";
-
-  const userData = getUserData();
-  localStorage.setItem("userData", JSON.stringify(userData));
-
-  // Clear the form after submission
-  document.getElementById("userForm").reset();
+    const [month, day, year] = parts.map(part => parseInt(part, 10));
+    const date = new Date(year, month - 1, day);
+    const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+    return formattedDate;
 }
 
-function getUserData() {
-  const table = document.querySelector("#userTable");
-  const tbody = table.tBodies[0];
+function validateDate() {
+    const dateInput = document.getElementById("dob");
+    const selectedDate = new Date(dateInput.value);
+    const maxDate = new Date(dateInput.max);
+    const minDate = new Date(dateInput.min);
 
-  const rows = Array.from(tbody.rows);
-  const userData = rows.map((row) => {
-    const [fullNameCell, emailAddressCell, passwordCell, dateOfBirthCell, acceptTermsCell] = row.cells;
-    return {
-      fullName: fullNameCell.textContent,
-      emailAddress: emailAddressCell.textContent,
-      password: passwordCell.textContent,
-      dateOfBirth: dateOfBirthCell.textContent,
-      acceptTerms: acceptTermsCell.textContent,
-    };
-  });
+    const isInvalid = selectedDate < minDate || selectedDate > maxDate;
 
-  return userData;
+    if (isInvalid) {
+        alert("Age should be between " + formatUserDate(dateInput.min) + " and " + formatUserDate(dateInput.max));
+    }
 }
 
-function validateEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+function setInitialState() {
+    const dateInput = document.getElementById("dob");
+    const today = new Date();
+    const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+    const minDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
+
+    dateInput.setAttribute("min", minDate.toISOString().split('T')[0]);
+    dateInput.setAttribute("max", maxDate.toISOString().split('T')[0]);
 }
 
-function validateAge(birthday) {
-  const givenBday = new Date(birthday);
-  const currentDate = new Date();
-  const age = calculateAge(currentDate, givenBday);
-  return age >= 18 && age <= 55;
-}
+window.onload = setInitialState;
 
-function calculateAge(dt2, dt1) {
-  const diff = (dt2.getTime() - dt1.getTime()) / 1000;
-  const age = Math.abs(Math.round(diff / (365.25 * 24 * 60 * 60)));
-  return age;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const registrationForm = document.getElementById("registrationForm");
+    const userTableBody = document.getElementById("userTableBody");
 
-window.onload = function () {
-  renderTable();
-};
+    loadUserEntries();
 
-function renderTable() {
-  const userData = JSON.parse(localStorage.getItem("userData"));
-  const tableBody = document.getElementById("tableBody");
+    registrationForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-  if (userData) {
-    const rows = userData
-      .map((data) => {
-        return `
-            <tr>
-                <td>${data.fullName}</td>
-                <td>${data.emailAddress}</td>
-                <td>${data.password}</td>
-                <td>${data.dateOfBirth}</td>
-                <td>${data.acceptTerms}</td>
-            </tr>`;
-      })
-      .join("");
+        const name = getValue("name");
+        const email = getValue("email");
+        const password = getValue("password");
+        const terms = getCheckedValue("terms");
+        const dob = getValue("dob");
 
+        const newRow = userTableBody.insertRow();
+        newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${password}</td><td>${dob}</td><td>${terms}</td>`;
+        clearFormFields();
+
+        saveUserEntry(name, email, password, dob, terms);
+    });
+
+    function getValue(id) {
+        return document.getElementById(id).value;
+    }
+
+    function getCheckedValue(id) {
+        return document.getElementById(id).checked;
+    }
+
+    function clearFormFields() {
+        registrationForm.reset();
+    }
+
+    function loadUserEntries() {
+        const userEntries = JSON.parse(localStorage.getItem("userEntries")) || [];
+        userEntries.forEach(({ name, email, password, dob, terms }) => {
+            const newRow = userTableBody.insertRow();
+            newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${password}</td><td>${dob}</td><td>${terms}</td>`;
+        });
+    }
+
+    function saveUserEntry(name, email, password, dob, terms) {
+        const userEntries = JSON.parse(localStorage.getItem("userEntries")) || [];
+        userEntries.push({ name, email, password, dob, terms });
+        localStorage.setItem("userEntries", JSON.stringify(userEntries));
+    }
+});
